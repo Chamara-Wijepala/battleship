b66bb021ffd9fa24c3d7d8a7ae716a2a746e97d8
Chamara-Wijepala <mr.worf777@gmail.com>
2021-12-31 17:15:00 +0530

    Add functions that update game by removing and re-creating a player's board

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import './index.css';
 import Player from './modules/classes/Player';
-import renderBoard from './modules/renderDom';
-
-const humanPlayer = new Player('Human');
-const computerPlayer = new Player('Computer');
+import {
+  renderBoard,
+  clearBoard,
+} from './modules/renderDom';
 
 const playerBoard = document.getElementById('player-board');
 const computerBoard = document.getElementById('computer-board');
 
-renderBoard(humanPlayer, playerBoard);
-renderBoard(computerPlayer, computerBoard);
+const humanPlayer = new Player('Human');
+const computerPlayer = new Player('Computer');
+
+function startGame() {
+  renderBoard(humanPlayer, playerBoard);
+  renderBoard(computerPlayer, computerBoard);
+}
+
+function updateGame(player, board) {
+  clearBoard(board);
+  renderBoard(player, board);
+}
+
+startGame();
+updateGame(humanPlayer, playerBoard);
--- src/modules/renderDom.js
+++ src/modules/renderDom.js
@@ -1,7 +1,18 @@
-export default function renderBoard(player, boardElement) {
+function renderBoard(player, boardElement) {
   for (let i = 0; i < player.gameBoard.board.length; i += 1) {
     const square = document.createElement('div');
     square.classList.add('square');
     boardElement.appendChild(square);
   }
 }
+
+function clearBoard(parent) {
+  while (parent.lastChild) {
+    parent.removeChild(parent.lastChild);
+  }
+}
+
+export {
+  renderBoard,
+  clearBoard,
+};
