48995e1a1327dd7f6abd0a7158657eaf86555de1
Chamara-Wijepala <mr.worf777@gmail.com>
2022-01-11 18:12:30 +0530

    Refactor checkCollisions function to fix bug with ships colliding at right edge

--- src/modules/placeShips.js
+++ src/modules/placeShips.js
@@ -20,7 +20,8 @@ function findSuitableStart(player, ship, direction) {
 // Creates an array of the locations a ship will be placed on and checks if it will collide with the
 // right edge or with other ships.
 function checkCollisions(start, ship, player, direction) {
-  const collisions = [9, 19, 29, 39, 49, 59, 69, 79, 89];
+  const rightCollisions = [9, 19, 29, 39, 49, 59, 69, 79, 89];
+  const leftCollisions = [10, 20, 30, 40, 50, 60, 70, 80, 90];
   const shipLength = [];
   for (let i = 0; i < ship.length; i += 1) {
     if (direction === 'Horizontal') {
@@ -30,12 +31,16 @@ function checkCollisions(start, ship, player, direction) {
     }
   }
   const collidesWithRightEdge = shipLength.some(
-    (value) => collisions.some((number) => number === value),
+    (value) => rightCollisions.some((number) => number === value),
+  );
+  const collidesWithLeftEdge = shipLength.some(
+    (value) => leftCollisions.some((number) => number === value),
   );
   const collidesWithShip = shipLength.some(
     (value) => player.gameBoard.board[value].hasShip,
   );
-  if (collidesWithShip === true || collidesWithRightEdge === true) { return true; }
+  if ((collidesWithRightEdge === true && collidesWithLeftEdge === true)
+    || (collidesWithShip === true)) { return true; }
   return false;
 }
 
